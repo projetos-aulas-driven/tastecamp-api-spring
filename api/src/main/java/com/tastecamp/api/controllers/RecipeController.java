b145816866efed5f4f0f93cd82f9411a12fe9326
leticia-chijo <leticia.chijo@gmail.com>
2024-07-23 15:03:27 -0300

    Aula - Métodos HTTP, params e body

--- api/src/main/java/com/tastecamp/api/controllers/RecipeController.java
+++ api/src/main/java/com/tastecamp/api/controllers/RecipeController.java
@@ -2,7 +2,13 @@ package com.tastecamp.api.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/recipes")
@@ -10,6 +16,27 @@ public class RecipeController {
     
    @GetMapping()
     public String getRecipes() {
-        return "Hello World";
-    }   
+        return "Lista de Receitas";
+    }
+
+    @GetMapping("/{id}")
+    public String getRecipeById(@PathVariable("id") Long id) {
+        return "Receita de id " + id;
+    }
+
+    @PostMapping()
+    public String createRecipe(@RequestBody String body) {
+        return body;
+    }
+
+    @PutMapping("/{id}")
+    public String updateRecipe(@PathVariable("id") Long id, @RequestBody String body) {
+        return "Edição da receita " + id + body;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteRecipe(@PathVariable("id") Long id) {
+        return "Deletando item " + id;
+    }
+    
 }
