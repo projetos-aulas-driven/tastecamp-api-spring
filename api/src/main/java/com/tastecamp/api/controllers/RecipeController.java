099feb9544c260f6d3f1a4876dfe5509676c786a
leticia-chijo <leticia.chijo@gmail.com>
2024-07-23 18:36:17 -0300

    Aula - Conexão com banco de dados (Spring Data JPA + Postgres)

--- api/src/main/java/com/tastecamp/api/controllers/RecipeController.java
+++ api/src/main/java/com/tastecamp/api/controllers/RecipeController.java
@@ -2,6 +2,7 @@ package com.tastecamp.api.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
---
+++ api/src/main/java/com/tastecamp/api/models/RecipeModel.java
@@ -0,0 +1,32 @@
+package com.tastecamp.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // cria getters e setters de tudo
+@AllArgsConstructor // cria um construtor com todos os argumentos da classe
+@NoArgsConstructor // cria um construtor sem argumentos
+@Entity // identifica o Model como uma entidade a ser mapeada no banco de dados
+@Table(name = "tb-recipes") // determinar o nome da tabela no banco
+public class RecipeModel {
+
+    @Id // identifica que é o id, a chave primária dessa tabela
+    @GeneratedValue(strategy = GenerationType.AUTO) // a estratégia de gração automática desse valor
+    private Long id;
+
+    @Column(length = 150, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String ingredients;
+
+    @Column(nullable = false)
+    private String steps;
+}
